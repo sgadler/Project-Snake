c7665497894041fba2376a6d9e479b7af4f233f6
Sebastian Gadler <gadlersebastian@gmail.com>
2021-08-11 17:04:46 +0200

    Started doing the menu but is still learning Raylib.

--- Project Snake/Program.cs
+++ Project Snake/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using Raylib_cs;
 
 namespace Project_Snake
 {
@@ -6,8 +9,22 @@ namespace Project_Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            int screenHeight = 750;
+            int screenWidth = 750;
+
+            Raylib.InitWindow(screenHeight,screenWidth, "Snake");
+
+            while(!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                Raylib.DrawText("Snake", 225,250,100,Color.BLACK);
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
         }
     }
 }
