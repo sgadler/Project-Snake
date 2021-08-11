40e9884dc3d8618bf873d8aa923adc5c5a21c5cf
Sebastian Gadler <gadlersebastian@gmail.com>
2021-08-12 01:17:18 +0200

    Menu Done, Game Start.

--- Project Snake/Program.cs
+++ Project Snake/Program.cs
@@ -8,10 +8,11 @@ namespace Project_Snake
 {
     class Program
     {
+
+        public static int screenHeight = 750;
+        public static int screenWidth = 750;
         static void Main(string[] args)
         {
-            int screenHeight = 750;
-            int screenWidth = 750;
 
             Raylib.InitWindow(screenHeight,screenWidth, "Snake");
 
@@ -28,19 +29,38 @@ namespace Project_Snake
                 Raylib.DrawText("Snake", 225,250,100,Color.BLACK);
                 Raylib.DrawRectangle(heightStart, widthStart, 160, 80,Color.GREEN);
                 Raylib.DrawRectangle(heightQuit, widthQuit, 160, 80,Color.RED);
-                Raylib.DrawText("Start",screenWidth/2 - 250, 550,50,Color.BLACK);
-                Raylib.DrawText("Quit",screenWidth/2 + 110,550,50,Color.BLACK);
-
-                if(Raylib.CheckCollisionPointRec(Raylib.GetMousePosition,))
-                {
-
-                }
-
+                Raylib.DrawText("Start(A)",screenWidth/2 - 250, 550,50,Color.BLACK);
+                Raylib.DrawText("Quit(D)",screenWidth/2 + 110,550,50,Color.BLACK);
 
                 Raylib.EndDrawing();
+
+                if(Raylib.IsKeyPressed(KeyboardKey.KEY_A) == true)
+                {
+                    Raylib.CloseWindow();
+                    SnakeGame();
+                }
+                if(Raylib.IsKeyPressed(KeyboardKey.KEY_D) == true)
+                {
+                    System.Environment.Exit(1);
+                }
             }
 
             Raylib.CloseWindow();
+        }
+
+        static void SnakeGame()
+        {
+            Raylib.InitWindow(screenHeight,screenWidth,"SnakeGame");
+
+            while(!Raylib.WindowShouldClose())
+            {
+            Raylib.BeginDrawing();
+
+            Raylib.ClearBackground(Color.GREEN);
+            Raylib.DrawText("Get Rekt", 1,1,100,Color.PINK);
+
+            Raylib.EndDrawing();
+            }
         }
     }
 }
